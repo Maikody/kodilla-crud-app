fc4f8d1e2352377fe3b9b8afb7c1d75258631029
Maikody <73773432+Maikody@users.noreply.github.com>
2021-02-17 13:30:00 +0100

    19.2 Mapper, repository, dbservice functionality added

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,28 +1,37 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("v1/task")
 public class TaskController {
 
-//    private TaskMapper taskMapper;
-//    private DbService dbService;
+    private TaskMapper taskMapper;
+    private DbService dbService;
+
+    public TaskController(TaskMapper taskMapper, DbService dbService) {
+        this.taskMapper = taskMapper;
+        this.dbService = dbService;
+    }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = dbService.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "Test title", "Test content");
+        Task foundTask = dbService.getTaskById(taskId);
+        return taskMapper.mapToTaskDto(foundTask);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
